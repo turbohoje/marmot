83354520aefd7d7af665ae3842ea1372d3277b52
Justin Meyer <jusmeyer@ebay.com>
2019-02-12 19:16:04 -0700

    fixing hook init

--- init-hooks.sh
+++ init-hooks.sh
@@ -3,6 +3,7 @@
 if [ -h .git/hooks/pre-commit ]; then
     echo "symlink exists, g2g"
 else
+    cd .git/hooks/
     echo "creating sym link"
-    ln -s pre-commit .git/hooks/pre-commit
+    ln -s ../../pre-commit pre-commit
 fi
